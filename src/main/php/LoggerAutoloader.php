0f6bfa910e3bca2aec8e7682dea656f0fb572fc9
Prashant Balan <pbalan@a-insight.com>
2014-01-10 16:41:27 +0800

    remove trigger error on autoload

--- src/main/php/LoggerAutoloader.php
+++ src/main/php/LoggerAutoloader.php
@@ -17,10 +17,11 @@
  * 
  * @package log4php
  */
-
-if (function_exists('__autoload')) {
-	trigger_error("log4php: It looks like your code is using an __autoload() function. log4php uses spl_autoload_register() which will bypass your __autoload() function and may break autoloading.", E_USER_WARNING);
-}
+if (function_exists('__autoload'))
+	spl_autoload_register('__autoload');
+// if (function_exists('__autoload')) {
+	// trigger_error("log4php: It looks like your code is using an __autoload() function. log4php uses spl_autoload_register() which will bypass your __autoload() function and may break autoloading.", E_USER_WARNING);
+// }
 
 spl_autoload_register(array('LoggerAutoloader', 'autoload'));
 
